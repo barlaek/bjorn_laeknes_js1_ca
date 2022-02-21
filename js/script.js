a934273497b43474e64e46a99e259cfba52891cd
Liridona <91525128+barlaek@users.noreply.github.com>
2022-02-21 21:24:15 +0100

    api key added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-fetch("https://api-nba-v1.p.rapidapi.com/leagues/", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-		"x-rapidapi-key": "9a5d0c3648msh3a060a92a7fc400p14adb4jsnf1a8864fe5bf"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+// fetch("https://api-nba-v1.p.rapidapi.com/leagues/", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+// 		"x-rapidapi-key": "9a5d0c3648msh3a060a92a7fc400p14adb4jsnf1a8864fe5bf"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
+
+const url = "https://api-nba-v1.p.rapidapi.com/players/league/Africa/?rapidapi-key=9a5d0c3648msh3a060a92a7fc400p14adb4jsnf1a8864fe5bf";
+
+const endpointContainer = document.querySelector(".endpoint");
+
+async function getLeagues() {
+    try {
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getLeagues();
