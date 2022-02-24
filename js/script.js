2fbcb76cb3476e4af3f218d2237639f805bfc5f7
Liridona <91525128+barlaek@users.noreply.github.com>
2022-02-24 22:18:56 +0100

    new API request

--- js/script.js
+++ js/script.js
@@ -12,20 +12,52 @@
 // 	console.error(err);
 // });
 
+// API KEY https://the-one-api.dev/v2/AzBkqSc29XPtYAnaQFMW
+
 const url = "https://api-nba-v1.p.rapidapi.com/players/league/Africa/?rapidapi-key=9a5d0c3648msh3a060a92a7fc400p14adb4jsnf1a8864fe5bf";
 
 const endpointContainer = document.querySelector(".endpoint");
 
-async function getLeagues() {
+async function getPlayers() {
     try {
         const response = await fetch(url);
 
         const data = await response.json();
 
         console.log(data);
+
+        const players = data;
+
+        endpointContainer.innerHTML = "";
+
+        for (let i = 0; i < players.length; i++) {
+            if (i === 3) {
+                break;
+            }
+        }
+
+        // const playerName = players[i].firstName;
+        // const teamId = players[i].teamId;
+        // const yearsPro = players[i].
     } catch (error) {
         console.log(error);
     }
 }
 
-getLeagues();
+getPlayers();
+
+
+// fetch("https://documenu.p.rapidapi.com/restaurant/4072702673999819", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-api-key": "7c2bf1469db11841bc00a0fa6886473e",
+// 		"x-rapidapi-host": "documenu.p.rapidapi.com",
+// 		"x-rapidapi-key": "9a5d0c3648msh3a060a92a7fc400p14adb4jsnf1a8864fe5bf"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
